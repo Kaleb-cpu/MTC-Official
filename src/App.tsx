580e0d11ed350612a2babcfbbd4f1539c5226616
Kaleb <kaleb.cpu@gmail.com>
2023-05-23 02:11:34 -0600

    routing created

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Nav from './pages/components/sections/Navbar'
 import Home from './pages/Home'
+import About from './pages/About'
+import AroundTowne from './pages/Around Towne';
+import ChildCare from './pages/Childcare';
+import FacilityRentals from './pages/Facility Rentals';
+import PageNotFound from './pages/PageNotFound';
 
 
 
-function App() {
+export default function App() {
   return (
-      <>
-          <div className="App">
-              <Nav/>
-              <Home/>
-              <hr/>
-              
-          </div>
-      </>
+      <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Nav />}>
+        <Route index element={<Home />} />
+        <Route path="FacilityRentals" element={<FacilityRentals />} />
+        <Route path="Childcare" element={<ChildCare />} />
+        <Route path="AroundTowne" element={<AroundTowne />} />
+        <Route path="About" element={<About />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
       );
   }
-      export default App;
+ 
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
--- src/pages/components/sections/Navbar.tsx
+++ src/pages/components/sections/Navbar.tsx
@@ -2,22 +2,20 @@ import { useState } from 'react'
 import Button from './Button';
 import logo from '/media/Logo.png';
 import '../../../assets/css files/App.css'
+import { Outlet, Link } from "react-router-dom";
 
-const navStyle = {
-  opacity: .6
-  
-}
+
 const Nav = () => {
-    let Links =[
-      {name:"Home",link:"/"},
-      {name:"Facility Rentals",link:"/"},
-      {name:"Childcare",link:"/"},
-      {name:"Around Towne",link:"/"},
-      {name:"About",link:"/"},
+    let links =[
+      {name:"Home",  to:"/"},
+      {name:"Facility Rentals", to:"/FacilityRentals"},
+      {name:"Childcare", to:"/Childcare"},
+      {name:"Around Towne", to:"/AroundTowne"},
+      {name:"About", to:"/About"},
     ];
     let [open,setOpen]=useState(false);
   return (
-    <div style={navStyle} className='shadow-md w-full fixed top-0 left-0'>
+    <div className='w-full top-0 left-0'>
       <div className='primary-background md:flex items-center justify-between py-4 md:px-10 px-7'>
       <div className=''>
         <img src={logo} className="App-logo" width={150} alt="logo" />
@@ -27,19 +25,15 @@ const Nav = () => {
       {/* <ion-icon name={open ? 'close':'menu'}></ion-icon> */}
       </div>
 
-      <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static text-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
-        {
-          Links.map((link)=>(
-            <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
-              <a href={link.link} className='font-bold hover:text-gray-400 duration-500'>{link.name}</a>
-            </li>
-          ))
-        }
-      </ul>
+
+      <div>
+         {links.map(link => (<Link to={link.to} className='pl-5 text-xl text-white'>{link.name}</Link> ))}
+      </div>
         <Button>
           Resident info
         </Button>
       </div>
+      <Outlet />
     </div>
   )
 }
