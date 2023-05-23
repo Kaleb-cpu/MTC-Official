1541f8a0782ad651cf4aed5d6e13b40e340a52a4
Kaleb <kaleb.cpu@gmail.com>
2023-05-22 20:39:27 -0600

    button prop bug fix

--- src/pages/components/sections/Button.tsx
+++ src/pages/components/sections/Button.tsx
@@ -1,6 +1,7 @@
+import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from "react"
 
 
-const Button = (props) => {
+const Button = (props: { children: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined }) => {
   return (
     <button className='bg-indigo-600 text-white font-[Poppins] py-2 px-6 rounded md:ml-8 hover:bg-indigo-400 
     duration-500 '>
