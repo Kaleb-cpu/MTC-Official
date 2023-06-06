f16983163e35feabbb7a65ced3626eb3050166bb
Kaleb <kaleb.cpu@gmail.com>
2023-06-05 20:14:42 -0600

    ice surface initial

--- src/pages/Childcare.tsx
+++ src/pages/Childcare.tsx
@@ -11,42 +11,60 @@ export default function ChildCare() {
       <p className='teritary font-2 text-xl text-left w-2/5 ml-5 mb-5'>MTC Childcare hours are from 7:00 am to 6:00 pm, Monday to Friday.</p>
       <h3 className='title teritary font-1 text-3xl text-center ml-5 mb-5' ><span className='span-title'>MTC Childcare</span> monthly fees</h3>
       
-      <section className='grid grid-cols-3 gap-3 ml-3 mr-3'>
-      {/* <!-- Start Single Service --> */}
-				<div>
-					<div className="single-service">
-						<div className="part-1">
+      <section className='plans'>
+      {/* <!-- types of plans container --> */}
+				
+					<div className="types-of-plans">
+						
+						<div className="option-1">
 							<i className="fab fa-500px"></i>
-							<h3 className="title">Type of Enrollment</h3>
-						</div>
-						<div className="part-2">
-							<p className="description">Express delivery inno service effective logistics solution for delivery of small cargo delivery service.</p>
-							<a href="#"><i className="fas fa-arrow-circle-right"></i>Read More</a>
-						</div>
+							<h3 className="title">Age Groups</h3>
+					<div className="age-group-1">
+						<h1>19 months - 2.4 years</h1>
 					</div>
-				</div>
-				{/* <!-- / End Single Service --> */}
-				{/* <!-- Start Single Service --> */}
-				<div>
-					<div className="single-service">
-						<div className="part-1">
+						</div>
+						<div className="option-1">
 							<i className="fab fa-angellist"></i>
-							<h3 className="title">Age Group</h3>
-						</div>
-						<div className="part-2">
-							<p className="description">Express delivery inno service effective logistics solution for delivery of small cargo delivery service.</p>
-							<a href="#"><i className="fas fa-arrow-circle-right"></i>Read More</a>
-						</div>
+							<h3 className="title">Types of Enrollments</h3>
+					<div className='age-1-enrollments text-left'>
+						
+						<li>Full-Time</li>
+						<li>Part-Time (3 days)</li>
+						<li>Part-Time (2 days)</li>
+						
 					</div>
-				</div>
-				{/* <!-- / End Single Service --> */}
-				{/* <!-- Start Single Service --> */}
-				<div>
-					<div className="single-service">
-						<div className="part-1">
+						</div>
+						<div className="option-1">
 							<i className="fas fa-award"></i>
 							<h3 className="title">Cost</h3>
+					<div className='age-1-cost text-left'>
+						<div className='option-1'>
+						<li>Resident: $1125</li>
+						<li>Non-Resident: $1175</li>
 						</div>
+<div>
+						<li className='option-2'>Resident: $675</li>
+						<li>Non-Resident: $705</li>
+						</div>
+
+<div>
+						<li className='option-3'>Resident: $450</li>
+						<li>Non-Resident: $470</li>
+						</div>
+					</div>
+						</div>
+					</div>
+
+					{/* plan b */}
+					
+					
+{/* first age group container */}
+				<div className='agc-1-container grid grid-cols-3 items-center '>
+				</div>
+				{/* <!-- / End Single Service --> */}
+				{/* <!-- Start Single Service --> */}
+				<div>
+					<div className="single-service">
 						<div className="part-2">
 							<p className="description">Express delivery inno service effective logistics solution for delivery of small cargo delivery service.</p>
 							<a href="#"><i className="fas fa-arrow-circle-right"></i>Read More</a>
--- src/pages/Ice Surface.tsx
+++ src/pages/Ice Surface.tsx
@@ -1,7 +1,34 @@
+import StormPond from '../../public/media/StormPond.png'
+
 export default function IceSurface() {
-    return (
-      <div>
-        <h1>This is the ice surface Page</h1>
-      </div>
-    );
-  }
+  return (
+    <div>
+      {/* main title */}
+      <h1 className="teritary display-5 font-1 mb-9 mt-10">Ice Surface</h1>
+
+      {/* prestwick common */}
+      <div className='teritary m-auto text-2xl font-2 bg-Primary w-max p-3 rounded-4'>
+      <h3 className='mb-2'><span className='fourth'>Prestwick Common</span> - 15113 Prestwick Blvd SE</h3>
+      <p className=' italic '>No hockey sticks or pucks are allowed on Prestwick Common.</p>
+
+</div>
+      {/* hockey ring */}
+      <h3>Hockey Rink - McKenzie Towne Gate & McKenzie Towne Link SE</h3>
+      {/* flood ice information */}
+      <h2>When does MTC flood ice surfaces?</h2>
+      <p>
+        The skating rinks will be flooded when operational capacity allows it
+        and when temperatures are below minus 10 degrees Celsius at the time of
+        flooding. Ice will be made on Prestwick Common and the hockey rink
+        surfaces by our Maintenance Department. *Please remember that this is
+        all dependent on the weather! Inverness and Prestwick Storm Ponds - DO
+        NOT SKATE on these surfaces. "Water levels and flows change rapidly,
+        making skating or other activities extremely dangerous and strictly
+        prohibited." - The City of Calgary, Water Services.
+      </p>
+      
+      <a href="/media/documents/Storm-Pond.pdf"><img src={StormPond} alt="storm-pond documentary" /></a>
+
+    </div>
+  );
+}
