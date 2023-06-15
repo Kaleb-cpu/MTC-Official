ea6b1831668d1c438563dcb09badadcfcd1ce7be
Kaleb <kaleb.cpu@gmail.com>
2023-06-15 16:18:57 -0600

    emergency added

--- src/pages/Emergency.tsx
+++ src/pages/Emergency.tsx
@@ -1,7 +1,84 @@
 export default function Emergency() {
     return (
       <div>
-        <h1>This is the Emergency Page</h1>
+         <h3 className="title teritary font-5 md:text-4xl text-2xl text-center ml-5 mb-5 mt-11">
+        <span className="span-title">Emergency </span>
+        Services
+      </h3>
+      <div>
+      <h3 className="teritary font-5 md:text-3xl text-xl text-center ml-5 mb-5 mt-11">
+        <span className="span-title">Fire Station #30 and EMS – 6 McKenzie Towne Gate SE </span>
+      </h3>
+      <h3 className="teritary font-5 md:text-2xl text-lg text-center ml-5 mb-5 mt-11">
+        <span className="span-title">District #8 Police – 450 Midpark Way SE </span>
+      </h3>
+      <ul className="text-center teritary text-lg ml-5">
+        <li>Ph: 403-428-6800</li>
+        <li>Ph: Emergency 911</li>
+        <li>Ph: Non-Emergency 403-266-1234</li>
+      </ul>
+      </div>
+
+      <div className="ContactLine">
+      <h3 className="teritary font-5 md:text-2xl text-lg text-left ml-5 mb-5 mt-11">
+        <span className="span-title">Health Link: </span>811
+      </h3>
+      
+
+     
+      <h3 className="teritary font-5 md:text-2xl text-lg text-left ml-5 mb-5 mt-11">
+        <span className="span-title">Kids Help Phone:  </span>1-866-332-2322 24Hrs
+      </h3>
+     
+
+      
+      <h3 className="teritary font-5 md:text-2xl text-lg text-left ml-5 mb-5 mt-11">
+        <span className="span-title">Addiction Services Helpline: </span>1-866-332-2322 24Hrs
+      </h3>
+      
+
+      
+      <h3 className="teritary font-5 md:text-2xl text-lg text-left ml-5 mb-5 mt-11">
+        <span className="span-title">Bullying Helpline: </span>1-888-456-2323 24Hrs  www.bullyfreealberta.ca
+      </h3>
+      
+
+      
+      <h3 className="teritary font-5 md:text-2xl text-lg text-left ml-5 mb-5 mt-11">
+        <span className="span-title">Child Abuse Helpline: </span>1-800-387-5437 24Hrs
+      </h3>
+      
+
+      
+      <h3 className="teritary font-5 md:text-2xl text-lg text-left ml-5 mb-5 mt-11">
+        <span className="span-title">Family Violence Info Line: </span>310-1818 24Hrs "Call if you (or someone you know) are going through family violence or abuse."
+      </h3>
+      
+
+      
+      <h3 className="teritary font-5 md:text-2xl text-lg text-left ml-5 mb-5 mt-11">
+        <span className="span-title">Distress Cente: </span>403-266-4357 24Hrs "We do not define crisis.  We do not judge. Anyone can call us day or night."
+      </h3>
+      
+
+      
+      <h3 className="teritary font-5 md:text-2xl text-lg text-left ml-5 mb-5 mt-11">
+        <span className="span-title">Access Mental Health: </span>1-844-943-1500  "Provides information, consultation and referral to individuals residing in Calgary who have addiction and/or mental health concerns."
+      </h3>
+      
+
+      
+      <h3 className="teritary font-5 md:text-2xl text-lg text-left ml-5 mb-5 mt-11">
+        <span className="span-title">Poison & Drug Information Services: </span>1-800-332-1414 24Hrs
+      </h3>
+      </div>
+
+
+
+
+
+
+
       </div>
     );
   }
